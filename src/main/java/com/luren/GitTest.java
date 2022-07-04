ab51c00195d455b908dbd96937d7c604dda1ac9a
luren <2489661630@qq.com>
2022-07-04 11:49:35 +0800

    first commit

---
+++ src/main/java/com/luren/GitTest.java
@@ -0,0 +1,14 @@
+package com.luren;
+
+/**
+ * @author admin
+ * @version 1.0.0
+ * @ClassName GitTest.java
+ * @Description TODO
+ * @createTime 2022年07月04日 11:44:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
